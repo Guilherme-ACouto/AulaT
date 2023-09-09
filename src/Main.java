154fc69b665c3d853f91d2a0c78e2d1a8c11c1b3
Guilherme-ACouto <couto.guilherme@pucpr.edu.br>
2023-09-09 18:16:40 -0300

    Refatorado novamente o main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     private static void sayHello() {
         System.out.println("Hello world!");
+        System.out.println("Olá mundo!");
+
     }
     public static void main(String[] args) {
         sayHello();
